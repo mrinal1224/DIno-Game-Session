dc4ad2781811d5bf20e6ed60c5ab99cc8fc40242
mrinal1224 <iammrinalkc@gmail.com>
2025-10-18 19:01:15 +0530

    push

--- script.js
+++ script.js
@@ -11,6 +11,26 @@ function jump() {
   }
 }
 
+const isAlive = setInterval(function(){
+   const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'))
+   const cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'))
+
+   // detect collision
+
+   if(cactusLeft<50 && cactusLeft>0 && dinoTop >=140){
+    alert("game Over Collison Detected")
+   }
+
+} , 10)
+
+
+
 document.addEventListener("keydown", function () {
   jump();
 });
+
+let score = 0;
+let scoreInterval = setInterval(() => {
+  score++;
+  document.getElementById("score").innerText = "Score: " + score;
+}, 1000);
